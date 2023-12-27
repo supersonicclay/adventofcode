db0d4febf0bf2ff1af1f382b17ebb314b888f5f6
Clay Anderson <supersonicclay@gmail.com>
2023-12-27 13:37:02 -0700

    WIP: 2023 Day 21 Part 2 better perf, but still not there

--- 2023/Day21/part2.js
+++ 2023/Day21/part2.js
@@ -13,35 +13,73 @@ const dirs = {
     'L': ([r, c]) => ([r, c-1]),
 }
 
-function visit(grid, pos, visited, steps, maxSteps) {
-    if (pos[0] < 0 || pos[0] >= grid.length) {
-        pos[0] = (pos[0]+grid.length) % grid.length;
+function clamp(pos, grid) {
+    const [r, c] = pos;
+    const clamped = [...pos];
+    const h = grid.length;
+    const w = grid[0].length;
+    // console.log(`=== r=${r}, c=${c}, h=${h}, w=${w}`)
+    if (r < 0 || r >= h) {
+        clamped[0] = (r+(h*(Math.abs(Math.ceil(r/h))+1))) % h;
     }
-    if (pos[1] < 0 || pos[1] >= grid[0].length) {
-        pos[1] = (pos[1]+grid[0].length) % grid[0].length;
+    if (c < 0 || c >= w) {
+        clamped[1] = (c+(w*(Math.abs(Math.ceil(c/w))+1))) % w;
+    }
+    return clamped;
+}
+
+function explore(grid, startingPos, maxSteps, visited) {
+
+    const oddDestinations = new Set();
+    const evenDestinations = new Set();
+
+    let thisStep = new Set();
+    thisStep.add(p2s(startingPos));
+    visited[0] = thisStep;
+
+    const start = new Date();
+
+    let nextStep;
+    for (let step=1; step<=maxSteps; step++) {
+        if (step%100===0) console.log(`step ${step}: ${step/(new Date()-start)*1000} steps/s`)
+        nextStep = new Set();
+        for (const s1 of thisStep.values()) {
+            for (const dir of Object.values(dirs)) {
+                const p1 = s1.split(',').map(Number);
+                const p2 = dir(p1);
+                const s2 = p2s(p2);
+
+                const clamped = clamp(p2, grid);
+                if (grid[clamped[0]][clamped[1]] === '#') {
+                    continue;
+                }
+
+                if (step % 2 === 0) {
+                    if (evenDestinations.has(s2)) {
+                        continue;
+                    }
+                    evenDestinations.add(s2);
+                } else {
+                    if (oddDestinations.has(s2)) {
+                        continue;
+                    }
+
+                    oddDestinations.add(s2);
+                }
+                nextStep.add(s2);
+            }
+        }
+        thisStep = nextStep;
+        // visited[step] = new Set([...thisStep]);
+        // if (step%2===0) {
+        //     [...evenDestinations.values()].forEach(d => visited[step].add(d));
+        // } else {
+        //     [...oddDestinations.values()].forEach(d => visited[step].add(d));
+        // }
     }
 
-    if (grid[pos[0]][pos[1]] === '#') {
-        return;
-    }
-
-    visited[steps] = visited[steps] ?? new Set();
-    if (visited[steps].has(p2s(pos))) {
-        // no need to process twice
-        return;
-    }
-    visited[steps].add(p2s(pos));
-
-    if (steps >= maxSteps) {
-        return;
-    }
-
-    for (const dir of Object.values(dirs)) {
-        visit(grid, dir(pos), visited, steps+1, maxSteps);
-    }
-    if (steps+1 < maxSteps) {
-        visited.delete(steps)
-    }
+    return maxSteps % 2 === 0 ? evenDestinations.size : oddDestinations.size;
+    // return visited[maxSteps].size;
 }
 
 function printGrid(grid, visited, step) {
@@ -63,25 +101,7 @@ function printGrid(grid, visited, step) {
     return `${gridString}\n${visited[step].size}@${step} steps`
 }
 
-function main(file, maxSteps) {
-    let result = 0;
-    const input = require('fs').readFileSync(file, 'utf-8');
-    const grid = input.split('\n').map(line => line.split(''));
-
-    let startCoordinate;
-    for (let r=0; r<grid.length && !startCoordinate; r++) {
-        for (let c=0; c<grid[0].length && !startCoordinate; c++) {
-            if (grid[r][c] === 'S') {
-                startCoordinate = [r, c];
-            }
-        }
-    }
-
-    const visited = new Map();
-    visit(grid, startCoordinate, visited, 0, maxSteps)
-    result = visited[maxSteps].size;
-
-
+function funDebug(grid, visited, maxSteps) {
     var screen = blessed.screen({
         smartCSR: true
     });
@@ -127,13 +147,44 @@ function main(file, maxSteps) {
     // Append our box to the screen.
     screen.append(box);
     screen.render();
+}
+
+function main(file, maxSteps) {
+    let result = 0;
+    const input = require('fs').readFileSync(file, 'utf-8');
+    const grid = input.split('\n').map(line => line.split(''));
+
+    let startCoordinate;
+    for (let r=0; r<grid.length && !startCoordinate; r++) {
+        for (let c=0; c<grid[0].length && !startCoordinate; c++) {
+            if (grid[r][c] === 'S') {
+                startCoordinate = [r, c];
+            }
+        }
+    }
+
+    visited = new Map();
+    result = explore(grid, startCoordinate, maxSteps, visited)
+
+    // funDebug(grid, visited, maxSteps);
 
     console.log(result);
 }
-// main('part1example.txt', 6);
-// console.log('expected 16');
+main('part1example.txt', 6);
+console.log('expected 16');
 main('part1example.txt', 10);
 console.log('expected 50');
+main('part1example.txt', 50);
+console.log('expected 1594');
+main('part1example.txt', 100);
+console.log('expected 6536');
+main('part1example.txt', 500);
+console.log('expected 167004');
+// main('part1example.txt', 1000);
+// console.log('expected 668697');
+// main('part1example.txt', 5000);
+// console.log('expected 16733044');
+
 // main('part1example.txt', 5000);
 // console.log('expected 16733044');
 // main('part1exercise.txt', 26501365); //
