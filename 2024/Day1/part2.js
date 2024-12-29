eb88d90b019a11f5cc335777d2f61381e19f37d4
Clay Anderson <supersonicclay@gmail.com>
2024-12-29 11:42:36 -0700

    Format Day1

--- 2024/Day1/part2.js
+++ 2024/Day1/part2.js
@@ -1,46 +1,50 @@
-
 function sum(x) {
-    return x.reduce((a,b)=>a+b, 0);
+  return x.reduce((a, b) => a + b, 0);
 }
 function mul(x) {
-    return x.reduce((a,b)=>a*b, 1);
+  return x.reduce((a, b) => a * b, 1);
 }
 
-const p2s = ([r,c]) => `${r},${c}`;
-const s2p = (s) => s.split(',').map(Number);
+const p2s = ([r, c]) => `${r},${c}`;
+const s2p = (s) => s.split(",").map(Number);
 
 const dirs = {
-    'U': ([r, c]) => ([r-1, c]),
-    'R': ([r, c]) => ([r, c+1]),
-    'D': ([r, c]) => ([r+1, c]),
-    'L': ([r, c]) => ([r, c-1]),
-}
-
+  U: ([r, c]) => [r - 1, c],
+  R: ([r, c]) => [r, c + 1],
+  D: ([r, c]) => [r + 1, c],
+  L: ([r, c]) => [r, c - 1],
+};
 
 function main(file) {
-    let result = 0;
-    const input = require('fs').readFileSync(file, 'utf-8');
-    const lines = input.split('\n').map(line =>
-        line.split(' ').map(x => x.trim()).filter(Boolean).map(Number));
+  let result = 0;
+  const input = require("fs").readFileSync(file, "utf-8");
+  const lines = input.split("\n").map((line) =>
+    line
+      .split(" ")
+      .map((x) => x.trim())
+      .filter(Boolean)
+      .map(Number)
+  );
 
-    const left = lines.map(line => line[0]);
-    const right = lines.reduce((acc, line) => {
-        const num = line[1];
-        if (acc[num]) {
-            acc[num]++;
-        } else {
-            acc[num] = 1;
-        }
-    return acc;
-    }, {});
-
-    for (let i = 0; i < left.length; i++) {
-        const l = left[i];
-        result += l * (right[l] ?? 0);
+  const left = lines.map((line) => line[0]);
+  const right = lines.reduce((acc, line) => {
+    const num = line[1];
+    if (acc[num]) {
+      acc[num]++;
+    } else {
+      acc[num] = 1;
     }
+    return acc;
+  }, {});
 
-    console.log(result);
+  for (let i = 0; i < left.length; i++) {
+    const l = left[i];
+    result += l * (right[l] ?? 0);
+  }
+
+  console.log(result);
 }
-main('part1example.txt');
-console.log('expected 31');
-main('part1exercise.txt'); // 27267728
+
+main("part1example.txt");
+console.log("expected 31");
+main("part1exercise.txt"); // 27267728
