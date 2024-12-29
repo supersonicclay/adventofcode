23411f90408a7bdd6efb2bccded51e5b751a6148
Clay Anderson <supersonicclay@gmail.com>
2024-12-29 11:46:29 -0700

    Add template and re-scaffold Days 2-25

--- 2024/scaffold-days.sh
+++ 2024/scaffold-days.sh
@@ -1,26 +1,52 @@
 #!/bin/sh
 
-cp -r Day1 Day2
-cp -r Day1 Day3
-cp -r Day1 Day4
-cp -r Day1 Day5
-cp -r Day1 Day6
-cp -r Day1 Day7
-cp -r Day1 Day8
-cp -r Day1 Day9
-cp -r Day1 Day10
-cp -r Day1 Day11
-cp -r Day1 Day12
-cp -r Day1 Day13
-cp -r Day1 Day14
-cp -r Day1 Day15
-cp -r Day1 Day16
-cp -r Day1 Day17
-cp -r Day1 Day18
-cp -r Day1 Day19
-cp -r Day1 Day20
-cp -r Day1 Day21
-cp -r Day1 Day22
-cp -r Day1 Day23
-cp -r Day1 Day24
-cp -r Day1 Day25
+
+rm -rf Day2
+rm -rf Day3
+rm -rf Day4
+rm -rf Day5
+rm -rf Day6
+rm -rf Day7
+rm -rf Day8
+rm -rf Day9
+rm -rf Day10
+rm -rf Day11
+rm -rf Day12
+rm -rf Day13
+rm -rf Day14
+rm -rf Day15
+rm -rf Day16
+rm -rf Day17
+rm -rf Day18
+rm -rf Day19
+rm -rf Day20
+rm -rf Day21
+rm -rf Day22
+rm -rf Day23
+rm -rf Day24
+rm -rf Day25
+
+cp -r Template Day2
+cp -r Template Day3
+cp -r Template Day4
+cp -r Template Day5
+cp -r Template Day6
+cp -r Template Day7
+cp -r Template Day8
+cp -r Template Day9
+cp -r Template Day10
+cp -r Template Day11
+cp -r Template Day12
+cp -r Template Day13
+cp -r Template Day14
+cp -r Template Day15
+cp -r Template Day16
+cp -r Template Day17
+cp -r Template Day18
+cp -r Template Day19
+cp -r Template Day20
+cp -r Template Day21
+cp -r Template Day22
+cp -r Template Day23
+cp -r Template Day24
+cp -r Template Day25
