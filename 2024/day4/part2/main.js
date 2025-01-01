f3c5f4a53cd1724f0a9fcd5df36b816988e22666
Clay Anderson <supersonicclay@gmail.com>
2024-12-31 21:36:18 -0700

    2024 Day 4 Part 2

--- 2024/day4/part2/main.js
+++ 2024/day4/part2/main.js
@@ -17,34 +17,59 @@ const dirs = {
   R: ([r, c]) => [r, c + 1],
   D: ([r, c]) => [r + 1, c],
   L: ([r, c]) => [r, c - 1],
+  UR: ([r, c]) => [r - 1, c + 1],
+  DR: ([r, c]) => [r + 1, c + 1],
+  DL: ([r, c]) => [r + 1, c - 1],
+  UL: ([r, c]) => [r - 1, c - 1],
 };
+
+const MAS = ["M", "A", "S"];
+
+function look(grid, pos, dir, index) {
+  if (index >= MAS.length) {
+    return true;
+  }
+
+  const [row, col] = pos;
+
+  if (row < 0 || row >= grid.length) {
+    return false;
+  }
+  if (col < 0 || col >= grid[row].length) {
+    return false;
+  }
+
+  if (grid[row][col] !== MAS[index]) {
+    return false;
+  }
+
+  return look(grid, dir(pos), dir, index + 1);
+}
 
 function main(file) {
   let result = 0;
   const input = require("fs").readFileSync(file, "utf-8");
-  const lines = input.split("\n").map((line) =>
-    line
-      .split(" ")
-      .map((x) => x.trim())
-      .filter(Boolean)
-      .map(Number)
-  );
+  const grid = input.split("\n").map((line) => line.split(""));
 
-  const left = lines.map((line) => line[0]);
-  const right = lines.map((line) => line[1]);
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[r].length; c++) {
+      const backslash =
+        look(grid, [r, c], dirs.DR, 0) ||
+        look(grid, [r + 2, c + 2], dirs.UL, 0);
+      const forwardslash =
+        look(grid, [r, c + 2], dirs.DL, 0) ||
+        look(grid, [r + 2, c], dirs.UR, 0);
 
-  left.sort((a, b) => a - b);
-  right.sort((a, b) => a - b);
-
-  for (let i = 0; i < left.length; i++) {
-    const l = left[i];
-    const r = right[i];
-    result += Math.abs(r - l);
+      if (backslash && forwardslash) {
+        // debug("found", r, c);
+        result++;
+      }
+    }
   }
 
   console.log(result);
 }
 
 main("example.txt");
-console.log("expected ___");
-// main("exercise.txt"); // ???
+console.log("expected 9");
+main("exercise.txt"); // 1737
