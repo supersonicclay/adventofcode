eeffe2a5a6354dac078559257a4047ebbb1a1063
Clay Anderson <supersonicclay@gmail.com>
2024-12-31 21:16:42 -0700

    2024 Day 3 Part 2

--- 2024/day3/part2/main.js
+++ 2024/day3/part2/main.js
@@ -19,32 +19,37 @@ const dirs = {
   L: ([r, c]) => [r, c - 1],
 };
 
+function getProduct(instructions) {
+  let result = 0;
+  const re = /mul\((\d{1,3})\,(\d{1,3})\)/g;
+
+  const numbers = [...instructions.matchAll(re)].map((m) =>
+    m.slice(1).map(Number)
+  );
+
+  for (let i = 0; i < numbers.length; i++) {
+    const [a, b] = numbers[i];
+    result += a * b;
+  }
+  return result;
+}
+
 function main(file) {
   let result = 0;
   const input = require("fs").readFileSync(file, "utf-8");
-  const lines = input.split("\n").map((line) =>
-    line
-      .split(" ")
-      .map((x) => x.trim())
-      .filter(Boolean)
-      .map(Number)
-  );
 
-  const left = lines.map((line) => line[0]);
-  const right = lines.map((line) => line[1]);
+  const parts = input.split("don't()");
 
-  left.sort((a, b) => a - b);
-  right.sort((a, b) => a - b);
+  result += getProduct(parts[0]);
 
-  for (let i = 0; i < left.length; i++) {
-    const l = left[i];
-    const r = right[i];
-    result += Math.abs(r - l);
+  for (let i = 1; i < parts.length; i++) {
+    const doInstructions = parts[i].split("do()").splice(1).join("");
+    result += getProduct(doInstructions);
   }
 
   console.log(result);
 }
 
 main("example.txt");
-console.log("expected ___");
-// main("exercise.txt"); // ???
+console.log("expected 48");
+main("exercise.txt"); // 62098619
