82c6a675fab4cb21b8e11e219122e5175372bf61
Clay Anderson <supersonicclay@gmail.com>
2025-01-01 18:49:08 -0700

    2024 Day 6 Part 1

--- 2024/day6/part1/main.js
+++ 2024/day6/part1/main.js
@@ -19,32 +19,54 @@ const dirs = {
   L: ([r, c]) => [r, c - 1],
 };
 
+const turnProcess = [dirs.U, dirs.R, dirs.D, dirs.L];
+
+function inBounds(grid, [r, c]) {
+  return r >= 0 && r < grid.length && c >= 0 && c < grid[0].length;
+}
+
 function main(file) {
   let result = 0;
   const input = require("fs").readFileSync(file, "utf-8");
-  const lines = input.split("\n").map((line) =>
-    line
-      .split(" ")
-      .map((x) => x.trim())
-      .filter(Boolean)
-      .map(Number)
-  );
+  const grid = input.split("\n").map((line) => line.split(""));
 
-  const left = lines.map((line) => line[0]);
-  const right = lines.map((line) => line[1]);
-
-  left.sort((a, b) => a - b);
-  right.sort((a, b) => a - b);
-
-  for (let i = 0; i < left.length; i++) {
-    const l = left[i];
-    const r = right[i];
-    result += Math.abs(r - l);
+  let turn = 0;
+  let dir = turnProcess[turn];
+  let pos;
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[r].length; c++) {
+      if (grid[r][c] === "^") {
+        pos = [r, c];
+        break;
+      }
+    }
   }
+
+  const visited = new Set();
+  while (inBounds(grid, pos)) {
+    visited.add(p2s(pos));
+    const cell = grid[pos[0]][pos[1]];
+
+    const next = dir(pos);
+    debug("next", next);
+    if (inBounds(grid, next)) {
+      const nextCell = grid[next[0]][next[1]];
+      if (nextCell === "#") {
+        turn++;
+        debug("turning", turn);
+        dir = turnProcess[turn % 4];
+        continue;
+      }
+    }
+    pos = next;
+  }
+
+  result = visited.size;
 
   console.log(result);
 }
 
+debug = noop;
 main("example.txt");
-console.log("expected ___");
-// main("exercise.txt"); // ???
+console.log("expected 41");
+main("exercise.txt"); // 5551
