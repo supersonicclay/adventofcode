f8475c97d297a3fc58e6f5ed079ab42a918b8e58
Clay Anderson <supersonicclay@gmail.com>
2025-01-01 19:23:49 -0700

    2024 Day 6 Part 2

--- 2024/day6/part2/main.js
+++ 2024/day6/part2/main.js
@@ -19,32 +19,73 @@ const dirs = {
   L: ([r, c]) => [r, c - 1],
 };
 
+const turnProcess = [dirs.U, dirs.R, dirs.D, dirs.L];
+
+function inBounds(grid, [r, c]) {
+  return r >= 0 && r < grid.length && c >= 0 && c < grid[0].length;
+}
+
+function doIGetStuck(grid, pos, turn) {
+  let dir = turnProcess[turn];
+
+  const visited = new Set();
+  while (inBounds(grid, pos)) {
+    debug("pos", pos, turn);
+    const visit = p2s(pos) + "|" + turn;
+    if (visited.has(visit)) {
+      return true;
+    }
+
+    visited.add(visit);
+    const next = dir(pos);
+    if (inBounds(grid, next)) {
+      const nextCell = grid[next[0]][next[1]];
+      if (nextCell === "#") {
+        turn = (turn + 1) % 4;
+        // debug("turning", turn);
+        dir = turnProcess[turn];
+        continue;
+      }
+    }
+    pos = next;
+  }
+  return false;
+}
+
 function main(file) {
   let result = 0;
   const input = require("fs").readFileSync(file, "utf-8");
-  const lines = input.split("\n").map((line) =>
-    line
-      .split(" ")
-      .map((x) => x.trim())
-      .filter(Boolean)
-      .map(Number)
-  );
+  const grid = input.split("\n").map((line) => line.split(""));
 
-  const left = lines.map((line) => line[0]);
-  const right = lines.map((line) => line[1]);
+  let turn = 0;
+  let pos;
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[r].length; c++) {
+      if (grid[r][c] === "^") {
+        pos = [r, c];
+        break;
+      }
+    }
+  }
 
-  left.sort((a, b) => a - b);
-  right.sort((a, b) => a - b);
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[r].length; c++) {
+      if (grid[r][c] !== ".") {
+        continue;
+      }
 
-  for (let i = 0; i < left.length; i++) {
-    const l = left[i];
-    const r = right[i];
-    result += Math.abs(r - l);
+      grid[r][c] = "#";
+      if (doIGetStuck(grid, pos, turn)) {
+        result++;
+      }
+      grid[r][c] = ".";
+    }
   }
 
   console.log(result);
 }
 
+debug = noop;
 main("example.txt");
-console.log("expected ___");
-// main("exercise.txt"); // ???
+console.log("expected 6");
+main("exercise.txt"); // ???
