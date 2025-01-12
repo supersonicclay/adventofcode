d8388f6456751ad623c16e220f1a66a34e4f5fc0
Clay Anderson <supersonicclay@gmail.com>
2025-01-11 22:07:30 -0700

    2024 Day 8 Part 2

--- 2024/day8/part2/main.js
+++ 2024/day8/part2/main.js
@@ -19,32 +19,86 @@ const dirs = {
   L: ([r, c]) => [r, c - 1],
 };
 
+function inBounds(pos, rows, cols) {
+  return pos[0] >= 0 && pos[0] < rows && pos[1] >= 0 && pos[1] < cols;
+}
+
+function travel(antinodes, k, [r, c], [dr, dc], rows, cols) {
+  debug("travel", k, r, c, dr, dc);
+  if (!inBounds([r, c], rows, cols)) {
+    return;
+  }
+  debug("adding");
+
+  // antinodes.add(p2s([r, c]) + String(Math.random()));
+  antinodes.add(p2s([r, c]));
+
+  travel(antinodes, k, [r + dr, c + dc], [dr, dc], rows, cols);
+}
+
 function main(file) {
   let result = 0;
   const input = require("fs").readFileSync(file, "utf-8");
-  const lines = input.split("\n").map((line) =>
-    line
-      .split(" ")
-      .map((x) => x.trim())
-      .filter(Boolean)
-      .map(Number)
-  );
+  const grid = input.split("\n").map((line) => line.split(""));
 
-  const left = lines.map((line) => line[0]);
-  const right = lines.map((line) => line[1]);
+  const rows = grid.length;
+  const cols = grid[0].length;
 
-  left.sort((a, b) => a - b);
-  right.sort((a, b) => a - b);
+  const map = new Map();
 
-  for (let i = 0; i < left.length; i++) {
-    const l = left[i];
-    const r = right[i];
-    result += Math.abs(r - l);
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      const cell = grid[r][c];
+      if (cell === ".") continue;
+
+      map.set(cell, map.get(cell) ?? []);
+      map.get(cell).push([r, c]);
+    }
   }
+
+  let antinodes = new Set();
+  for (const [k, coords] of map) {
+    for (let i = 0; i < coords.length; i++) {
+      for (let j = i + 1; j < coords.length; j++) {
+        const [r1, c1] = coords[i];
+        const [r2, c2] = coords[j];
+
+        // if (r1 !== 2 || c1 !== 5 || r2 !== 3 || c2 !== 7) {
+        //   continue;
+        // }
+
+        debug("=============", k, [r1, c1], [r2, c2]);
+
+        let dr = r1 - r2;
+        let dc = c1 - c2;
+        travel(antinodes, k, [r1, c1], [dr, dc], rows, cols);
+
+        dr = r2 - r1;
+        dc = c2 - c1;
+        travel(antinodes, k, [r2, c2], [dr, dc], rows, cols);
+      }
+    }
+  }
+
+  for (let r = 0; r < rows; r++) {
+    debug(
+      grid[r]
+        .map((x, c) => {
+          if (antinodes.has(p2s([r, c]))) {
+            return "#";
+          }
+          return x;
+        })
+        .join("")
+    );
+  }
+  debug(antinodes);
+  result = antinodes.size;
 
   console.log(result);
 }
 
+debug = noop;
 main("example.txt");
-console.log("expected ___");
-// main("exercise.txt"); // ???
+console.log("expected 34");
+main("exercise.txt"); // 1064
