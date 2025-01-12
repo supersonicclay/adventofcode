49a12f1876881f9733714dfbd7e114fa70d3ada7
Clay Anderson <supersonicclay@gmail.com>
2025-01-11 21:35:04 -0700

    2024 Day 8 Part 1

--- 2024/day8/part1/main.js
+++ 2024/day8/part1/main.js
@@ -19,32 +19,59 @@ const dirs = {
   L: ([r, c]) => [r, c - 1],
 };
 
+function inBounds(pos, rows, cols) {
+  return pos[0] >= 0 && pos[0] < rows && pos[1] >= 0 && pos[1] < cols;
+}
+
 function main(file) {
   let result = 0;
   const input = require("fs").readFileSync(file, "utf-8");
-  const lines = input.split("\n").map((line) =>
-    line
-      .split(" ")
-      .map((x) => x.trim())
-      .filter(Boolean)
-      .map(Number)
-  );
+  const grid = input.split("\n").map((line) => line.split(""));
 
-  const left = lines.map((line) => line[0]);
-  const right = lines.map((line) => line[1]);
+  const rows = grid.length;
+  const cols = grid[0].length;
 
-  left.sort((a, b) => a - b);
-  right.sort((a, b) => a - b);
+  const map = new Map();
 
-  for (let i = 0; i < left.length; i++) {
-    const l = left[i];
-    const r = right[i];
-    result += Math.abs(r - l);
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      const cell = grid[r][c];
+      if (cell === ".") continue;
+
+      map.set(cell, map.get(cell) ?? []);
+      map.get(cell).push([r, c]);
+    }
   }
+
+  let antinodes = new Set();
+  for (const [k, coords] of map) {
+    for (let i = 0; i < coords.length; i++) {
+      for (let j = i + 1; j < coords.length; j++) {
+        const [r1, c1] = coords[i];
+        const [r2, c2] = coords[j];
+
+        const antinode1 = [r1 + r1 - r2, c1 + c1 - c2];
+        const antinode2 = [r2 + r2 - r1, c2 + c2 - c1];
+
+        debug(k, [r1, c1], [r2, c2], antinode1, antinode2);
+
+        if (inBounds(antinode1, rows, cols)) {
+          antinodes.add(p2s(antinode1));
+        }
+        if (inBounds(antinode2, rows, cols)) {
+          antinodes.add(p2s(antinode2));
+        }
+      }
+    }
+  }
+
+  debug(antinodes);
+  result = antinodes.size;
 
   console.log(result);
 }
 
+debug = noop;
 main("example.txt");
-console.log("expected ___");
-// main("exercise.txt"); // ???
+console.log("expected 14");
+main("exercise.txt"); // 313
