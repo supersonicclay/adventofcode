a90d127c2e1645a39afe407c95dbdd34c16b9429
Clay Anderson <supersonicclay@gmail.com>
2025-01-12 20:17:51 -0700

    WIP: 2024 Day 9 Part 2
    example working, but not full exercise

--- 2024/day9/part2/main.js
+++ 2024/day9/part2/main.js
@@ -19,32 +19,73 @@ const dirs = {
   L: ([r, c]) => [r, c - 1],
 };
 
+function joinDisk(diskParts) {
+  const disk = [];
+  for (let i = 0; i < diskParts.length; i++) {
+    const { fileId, size, gap } = diskParts[i];
+
+    for (let bit = 0; bit < size; bit++) {
+      disk.push(fileId);
+    }
+    for (let bit = 0; bit < gap; bit++) {
+      disk.push(".");
+    }
+  }
+  debug(disk.join(""));
+  return disk;
+}
+
 function main(file) {
   let result = 0;
   const input = require("fs").readFileSync(file, "utf-8");
-  const lines = input.split("\n").map((line) =>
-    line
-      .split(" ")
-      .map((x) => x.trim())
-      .filter(Boolean)
-      .map(Number)
-  );
+  const nums = input.split("").map(Number);
 
-  const left = lines.map((line) => line[0]);
-  const right = lines.map((line) => line[1]);
+  let diskParts = [];
+  for (let i = 0; i < nums.length; i += 2) {
+    const [size, gap] = nums.slice(i, i + 2);
+    diskParts.push({ fileId: i / 2, size, gap: gap ?? 0 });
+  }
+  joinDisk(diskParts);
+  debug("-----");
 
-  left.sort((a, b) => a - b);
-  right.sort((a, b) => a - b);
+  for (let i = diskParts.length - 1; i > 0; i--) {
+    const {
+      fileId: fileIdToMove,
+      size: sizeToMove,
+      gap: gapToMove,
+    } = diskParts[i];
+    for (let j = 0; j < i; j++) {
+      const { size, gap } = diskParts[j];
+      if (gap >= sizeToMove) {
+        // debug("move", i, diskParts[i], "to", j, diskParts[j]);
 
-  for (let i = 0; i < left.length; i++) {
-    const l = left[i];
-    const r = right[i];
-    result += Math.abs(r - l);
+        diskParts[i].gap += sizeToMove;
+        diskParts[i].size = 0;
+        diskParts[j].gap = 0;
+
+        diskParts.splice(j + 1, 0, {
+          fileId: fileIdToMove,
+          size: sizeToMove,
+          gap: gap - sizeToMove,
+        });
+        break;
+      }
+    }
+    joinDisk(diskParts);
+  }
+
+  const disk = joinDisk(diskParts);
+
+  for (let i = 0; i < disk.length; i++) {
+    if (disk[i] !== ".") {
+      result += i * disk[i];
+    }
   }
 
   console.log(result);
 }
 
+// debug = noop;
 main("example.txt");
-console.log("expected ___");
-// main("exercise.txt"); // ???
+console.log("expected 2858");
+// main("exercise.txt"); // 9870177277851 is too high
