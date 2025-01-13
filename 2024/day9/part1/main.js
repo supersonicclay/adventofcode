c3f04dbd7d5fc24d6d1f3470dca7e180c868acd5
Clay Anderson <supersonicclay@gmail.com>
2025-01-12 17:03:07 -0700

    2024 Day 9 Part 1

--- 2024/day9/part1/main.js
+++ 2024/day9/part1/main.js
@@ -22,29 +22,47 @@ const dirs = {
 function main(file) {
   let result = 0;
   const input = require("fs").readFileSync(file, "utf-8");
-  const lines = input.split("\n").map((line) =>
-    line
-      .split(" ")
-      .map((x) => x.trim())
-      .filter(Boolean)
-      .map(Number)
-  );
+  const nums = input.split("").map(Number);
 
-  const left = lines.map((line) => line[0]);
-  const right = lines.map((line) => line[1]);
+  const disk = [];
+  for (let i = 0; i < nums.length; i += 2) {
+    const [size, gap] = nums.slice(i, i + 2);
 
-  left.sort((a, b) => a - b);
-  right.sort((a, b) => a - b);
+    for (let bit = 0; bit < size; bit++) {
+      disk.push(i / 2);
+    }
+    for (let bit = 0; bit < gap; bit++) {
+      disk.push(".");
+    }
+  }
 
-  for (let i = 0; i < left.length; i++) {
-    const l = left[i];
-    const r = right[i];
-    result += Math.abs(r - l);
+  let left = 0;
+  let right = disk.length - 1;
+  while (left < right) {
+    if (disk[right] === ".") {
+      right--;
+      continue;
+    }
+    if (disk[left] !== ".") {
+      left++;
+      continue;
+    }
+
+    disk[left] = disk[right];
+    disk[right] = ".";
+    debug(disk.join(""));
+  }
+
+  for (let i = 0; i < disk.length; i++) {
+    if (disk[i] !== ".") {
+      result += i * disk[i];
+    }
   }
 
   console.log(result);
 }
 
+debug = noop;
 main("example.txt");
-console.log("expected ___");
-// main("exercise.txt"); // ???
+console.log("expected 1928");
+main("exercise.txt"); // 6386640365805
