d3ba4160535dd73db377692eedc035bdb3fd986e
Clay Anderson <supersonicclay@gmail.com>
2025-01-18 09:53:26 -0700

    2024 Day 11 Part 1

--- 2024/day11/part1/main.js
+++ 2024/day11/part1/main.js
@@ -19,32 +19,48 @@ const dirs = {
   L: ([r, c]) => [r, c - 1],
 };
 
+function blink(nums, numBlinks) {
+  for (let b = 0; b < numBlinks; b++) {
+    const newNums = [];
+    let j = 0;
+    for (let i = 0; i < nums.length; i++) {
+      const n = nums[i];
+      if (n === 0) {
+        newNums.push(1);
+      } else {
+        const s = String(n);
+        if (s.length % 2 == 0) {
+          const n1 = Number(s.slice(0, s.length / 2));
+          const n2 = Number(s.slice(s.length / 2));
+          debug("even", n, n1, n2);
+          newNums.push(n1);
+          newNums.push(n2);
+          j++;
+        } else {
+          newNums.push(n * 2024);
+        }
+      }
+      j++;
+    }
+    debug("blink", b, "---", newNums.join(" "));
+    nums = newNums;
+  }
+  return nums.length;
+}
+
 function main(file) {
   let result = 0;
   const input = require("fs").readFileSync(file, "utf-8");
-  const lines = input.split("\n").map((line) =>
-    line
-      .split(" ")
-      .map((x) => x.trim())
-      .filter(Boolean)
-      .map(Number)
-  );
+  const nums = input.split(" ").map(Number);
 
-  const left = lines.map((line) => line[0]);
-  const right = lines.map((line) => line[1]);
+  debug(nums);
 
-  left.sort((a, b) => a - b);
-  right.sort((a, b) => a - b);
-
-  for (let i = 0; i < left.length; i++) {
-    const l = left[i];
-    const r = right[i];
-    result += Math.abs(r - l);
-  }
+  result = blink(nums, 25);
 
   console.log(result);
 }
 
+debug = noop;
 main("example.txt");
-console.log("expected ___");
-// main("exercise.txt"); // ???
+console.log("expected 55312");
+main("exercise.txt"); // 193269
